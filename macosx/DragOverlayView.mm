e185c47ad39f9238a32ae0f660f4bada0f8ee686
Dmitry Lobanov <gaussblurinc@gmail.com>
2026-09-05 20:12:12 +0300

    refactor: migrate drag overlay view to appkit views. (#261)

--- macosx/DragOverlayView.mm
+++ macosx/DragOverlayView.mm
@@ -4,15 +4,28 @@
 
 #import "DragOverlayView.h"
 
-static CGFloat const kPadding = 10.0;
-static CGFloat const kIconWidth = 64.0;
+// Layout
+
+// Badge
+static CGFloat const kBadgeWidth = 325.0;
+static CGFloat const kBadgeHeight = 84.0;
+static CGFloat const kBadgeCornerRadius = 15.0;
+
+// MainStack
+static CGFloat const kMainStackInset = 10.0;
+static CGFloat const kMainStackSpacing = 5.0;
+
+// Icon
+static CGFloat const kIconSize = 64.0;
+
+// TextStack
+static CGFloat const kTextStackSpacing = 2.0;
 
 @interface DragOverlayView ()
 
-@property(nonatomic) NSImage* fBadge;
-
-@property(nonatomic, readonly) NSDictionary* fMainLineAttributes;
-@property(nonatomic, readonly) NSDictionary* fSubLineAttributes;
+@property(nonatomic, readonly) NSImageView* fIconImageView;
+@property(nonatomic, readonly) NSTextField* fTitleLabel;
+@property(nonatomic, readonly) NSTextField* fSubtitleLabel;
 
 @end
 
@@ -21,81 +34,71 @@ static CGFloat const kIconWidth = 64.0;
 - (instancetype)initWithFrame:(NSRect)frame
 {
     if ((self = [super initWithFrame:frame])) {
-        //create attributes
-        NSShadow* stringShadow = [[NSShadow alloc] init];
-        stringShadow.shadowOffset = NSMakeSize(2.0, -2.0);
-        stringShadow.shadowBlurRadius = 4.0;
+        NSView* badgeContainer = [[NSView alloc] initWithFrame:NSZeroRect];
+        badgeContainer.translatesAutoresizingMaskIntoConstraints = NO;
+        badgeContainer.wantsLayer = YES;
+        badgeContainer.layer.backgroundColor = [NSColor colorWithCalibratedWhite:0.0 alpha:0.75].CGColor;
+        badgeContainer.layer.cornerRadius = kBadgeCornerRadius;
+        [self addSubview:badgeContainer];
 
-        NSFont *bigFont = [NSFont boldSystemFontOfSize:18.0], *smallFont = [NSFont systemFontOfSize:14.0];
+        _fIconImageView = [[NSImageView alloc] initWithFrame:NSZeroRect];
+        _fIconImageView.imageScaling = NSImageScaleProportionallyUpOrDown;
 
-        NSMutableParagraphStyle* paragraphStyle = [NSParagraphStyle.defaultParagraphStyle mutableCopy];
-        paragraphStyle.lineBreakMode = NSLineBreakByTruncatingMiddle;
+        _fTitleLabel = [NSTextField labelWithString:@""];
+        _fTitleLabel.font = [NSFont boldSystemFontOfSize:18.0];
 
-        _fMainLineAttributes = @{
-            NSForegroundColorAttributeName : NSColor.whiteColor,
-            NSFontAttributeName : bigFont,
-            NSShadowAttributeName : stringShadow,
-            NSParagraphStyleAttributeName : paragraphStyle
-        };
+        _fSubtitleLabel = [NSTextField labelWithString:@""];
+        _fSubtitleLabel.font = [NSFont systemFontOfSize:14.0];
 
-        _fSubLineAttributes = @{
-            NSForegroundColorAttributeName : NSColor.whiteColor,
-            NSFontAttributeName : smallFont,
-            NSShadowAttributeName : stringShadow,
-            NSParagraphStyleAttributeName : paragraphStyle
-        };
+        for (NSTextField* label in @[ _fTitleLabel, _fSubtitleLabel ]) {
+            label.textColor = NSColor.whiteColor;
+            label.maximumNumberOfLines = 1;
+            label.lineBreakMode = NSLineBreakByTruncatingMiddle;
+        }
+
+        NSStackView* textStackView = [NSStackView stackViewWithViews:@[ _fTitleLabel, _fSubtitleLabel ]];
+        textStackView.orientation = NSUserInterfaceLayoutOrientationVertical;
+        textStackView.alignment = NSLayoutAttributeLeading;
+        textStackView.spacing = kTextStackSpacing;
+
+        // Prevent the vertical stack from stretching awkwardly in height
+        [textStackView setContentHuggingPriority:NSLayoutPriorityDefaultHigh forOrientation:NSLayoutConstraintOrientationVertical];
+
+        NSStackView* mainStackView = [NSStackView stackViewWithViews:@[ _fIconImageView, textStackView ]];
+        mainStackView.translatesAutoresizingMaskIntoConstraints = NO;
+        mainStackView.orientation = NSUserInterfaceLayoutOrientationHorizontal;
+        mainStackView.alignment = NSLayoutAttributeCenterY;
+        mainStackView.spacing = kMainStackSpacing;
+
+        // AppKit NSStackView edgeInsets push views INWARD, so we use positive numbers here
+        mainStackView.edgeInsets = NSEdgeInsetsMake(0, kMainStackInset, 0, kMainStackInset);
+
+        [badgeContainer addSubview:mainStackView];
+
+        [NSLayoutConstraint activateConstraints:@[
+            [badgeContainer.centerXAnchor constraintEqualToAnchor:self.centerXAnchor],
+            [badgeContainer.centerYAnchor constraintEqualToAnchor:self.centerYAnchor],
+
+            [badgeContainer.widthAnchor constraintEqualToConstant:kBadgeWidth],
+            [badgeContainer.heightAnchor constraintEqualToConstant:kBadgeHeight],
+
+            [_fIconImageView.widthAnchor constraintEqualToConstant:kIconSize],
+            [_fIconImageView.heightAnchor constraintEqualToConstant:kIconSize],
+
+            [mainStackView.leadingAnchor constraintEqualToAnchor:badgeContainer.leadingAnchor],
+            [mainStackView.trailingAnchor constraintEqualToAnchor:badgeContainer.trailingAnchor],
+            [mainStackView.topAnchor constraintEqualToAnchor:badgeContainer.topAnchor],
+            [mainStackView.bottomAnchor constraintEqualToAnchor:badgeContainer.bottomAnchor]
+        ]];
     }
     return self;
 }
 
 - (void)setOverlay:(NSImage*)icon mainLine:(NSString*)mainLine subLine:(NSString*)subLine
 {
-    //create badge
-    NSRect const badgeRect = NSMakeRect(0.0, 0.0, 325.0, 84.0);
-
-    self.fBadge = [[NSImage alloc] initWithSize:badgeRect.size];
-    [self.fBadge lockFocus];
-
-    NSBezierPath* bp = [NSBezierPath bezierPathWithRoundedRect:badgeRect xRadius:15.0 yRadius:15.0];
-    [[NSColor colorWithCalibratedWhite:0.0 alpha:0.75] set];
-    [bp fill];
-
-    //place icon
-    [icon drawInRect:NSMakeRect(kPadding, (NSHeight(badgeRect) - kIconWidth) * 0.5, kIconWidth, kIconWidth) fromRect:NSZeroRect
-           operation:NSCompositingOperationSourceOver
-            fraction:1.0];
-
-    //place main text
-    NSSize const mainLineSize = [mainLine sizeWithAttributes:self.fMainLineAttributes];
-    NSSize const subLineSize = [subLine sizeWithAttributes:self.fSubLineAttributes];
-
-    NSRect lineRect = NSMakeRect(
-        kPadding + kIconWidth + 5.0,
-        (NSHeight(badgeRect) + (subLineSize.height + 2.0 - mainLineSize.height)) * 0.5,
-        NSWidth(badgeRect) - (kPadding + kIconWidth + 2.0) - kPadding,
-        mainLineSize.height);
-    [mainLine drawInRect:lineRect withAttributes:self.fMainLineAttributes];
-
-    //place sub text
-    lineRect.origin.y -= subLineSize.height + 2.0;
-    lineRect.size.height = subLineSize.height;
-    [subLine drawInRect:lineRect withAttributes:self.fSubLineAttributes];
-
-    [self.fBadge unlockFocus];
-
-    self.needsDisplay = YES;
-}
-
-- (void)drawRect:(NSRect)rect
-{
-    if (self.fBadge) {
-        NSRect const frame = self.frame;
-        NSSize const imageSize = self.fBadge.size;
-        [self.fBadge drawAtPoint:NSMakePoint((NSWidth(frame) - imageSize.width) * 0.5, (NSHeight(frame) - imageSize.height) * 0.5)
-                        fromRect:NSZeroRect
-                       operation:NSCompositingOperationSourceOver
-                        fraction:1.0];
-    }
+    self.fIconImageView.image = icon;
+    self.fTitleLabel.stringValue = mainLine ?: @"";
+    self.fSubtitleLabel.stringValue = subLine ?: @"";
 }
 
 @end
